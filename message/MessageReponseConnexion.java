37ea7de9c5a81e427a9a515d2cb9d204a56e6024
Olzindel <timo.thibault@gmail.com>
2017-12-23 15:11:11 +0100

    Ajout utilisateur dans MessageReponseConnexion

--- message/MessageReponseConnexion.java
+++ message/MessageReponseConnexion.java
@@ -3,14 +3,20 @@ package message;
 public class MessageReponseConnexion extends Message {
 	private static final long serialVersionUID = 1L;
 	private boolean accepte;
+	private Utilisateur utilisateur
 	
-	public MessageReponseConnexion(String adresseIp, boolean accepte){
+	public MessageReponseConnexion(String adresseIp, boolean accepte, Utilisateur utilisateur){
 		super(adresseIp);
 		this.accepte=accepte;
+		this.utilisateur = utilisateur;
 	}
 
 	public boolean getAccepte() {
 		return this.accepte;
+	}
+	
+	public Utilisateur getUtilisateur(){
+		return utilisateur;
 	}
 	
 }
