ce5ffc20916543379ae281c8c2e354c5ba152b2e
Sakdow <nathan.31@hotmail.fr>
2017-12-26 15:26:02 +0100

    V1 du réseau client

---
+++ client/ThreadEcoute.java
@@ -0,0 +1,65 @@
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import message.MessageDeconnexion;
+import message.MessageDemConnexion;
+
+public class ThreadEcoute {
+	Socket socket;
+	static final String serverName = "localhost";
+	static final int serverPort = 9999;
+
+	public void connexionServeur(String idUtilisateur, String motDePasse) {
+		try {
+			socket = new Socket(serverName, serverPort);
+
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
+
+			MessageDemConnexion messageDemConnexion = new MessageDemConnexion(idUtilisateur, motDePasse);
+
+			out.writeObject(messageDemConnexion);
+			out.flush();
+
+			out.close();
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	public void ecoute() {
+
+	}
+
+	public void deconnexionServeur(String idUtilisateur) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
+
+			MessageDeconnexion messageDeconnexion = new MessageDeconnexion(idUtilisateur);
+
+			out.writeObject(messageDeconnexion);
+			out.flush();
+
+			out.close();
+			socket.close();
+		} catch (UnknownHostException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+}
