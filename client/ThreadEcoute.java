908187707badf5ed31dea84ef842e9c8a0ffad99
Sakdow <nathan.31@hotmail.fr>
2018-01-08 14:11:17 +0100

    Gestion suppression groupe

--- client/ThreadEcoute.java
+++ client/ThreadEcoute.java
@@ -19,19 +19,17 @@ public class ThreadEcoute extends Thread {
 	public void run() {
 		while (!Thread.interrupted()) {
 			Message messageRecu = reseaux.ecoute();
-			try {
-				Thread.sleep(3);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			if (messageRecu != null) {
 				if (messageRecu instanceof MessageTicket) {
 					MessageTicket messageTicket = (MessageTicket) messageRecu;
 					client.ajouterTicket(messageTicket.getTicket());
 				} else if (messageRecu instanceof MessageGroupe) {
 					MessageGroupe messageGroupe = (MessageGroupe) messageRecu;
-					client.ajouterGroupe(messageGroupe.getGroupe());
+					if(messageGroupe.isAjouter()){
+						client.ajouterGroupe(messageGroupe.getGroupe());
+					} else {
+						client.supprimerGroupe(messageGroupe.getGroupe());
+					}
 				} else {
 					MessageMessageConversation nouveauMessConv = (MessageMessageConversation) messageRecu;
 					client.ajouterMessageConv(nouveauMessConv.getIdTicket(), nouveauMessConv.getIdGroupe(),
